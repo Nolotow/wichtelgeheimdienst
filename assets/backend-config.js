57d045363bac6888faf517ef3d7d339ce54a279c
Nolotow <maxxheeb@web.de>
2026-09-08 23:50:51 +0200

    Delete assets directory

--- assets/backend-config.js
+++
@@ -1,3 +0,0 @@
-// Nach dem Erstellen des Cloudflare Workers hier dessen URL eintragen.
-// Beispiel: https://wwg-backend.DEINNAME.workers.dev
-window.WWG_BACKEND_URL = "https://wwg-backend.maxxheeb.workers.dev";
